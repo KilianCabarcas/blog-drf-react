c3d3f982498bd2f96accd6e2993254663d550592
KilianCabarcas <kiliancabarcas09@gmail.com>
2024-07-23 13:36:00 -0500

    configuration core

---
+++ core/storage_backends.py
@@ -0,0 +1,9 @@
+from storage.backends.s3boto import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'private'
+
+class MediaStore(S3Boto3Storage):
+    location = "media"
+    file_overwrite = False
